d9f2aa17bfe5cb59ff6201376145f857232d7e10
hhh131 <gmlrnjs131@naver.com>
2023-01-12 01:29:35 +0900

    Initial Commit

---
+++ ChatList/ChatListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by 신희권 on 2023/01/12.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
